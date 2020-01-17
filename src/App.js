9829eef69919dac2127fea50ac51aaf5c7cdf144
Jimmy <bruno_pier@hotmail.com>
2020-01-17 14:31:26 -0300

    Adding 1st component - test - App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function Hello (props){
+  return <h2>{props.title}</h2>
+}
+
 class App extends Component{
   render(){
     return (
       <div className="App">
         <div className="App-header">
           <img src="" alt=""/>
-          <h2>Bienvenidos al curso</h2>
+          <Hello title='Hello from props'/>
         </div>
         <h3>Aprendiendo React</h3>
         <p>Hola mundo</p>
