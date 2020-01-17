9e1419e3b90c7fe8d64a76f73fa44cb66c5bad18
Jimmy <bruno_pier@hotmail.com>
2020-01-17 13:44:17 -0300

    First renderings - App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src="" alt=""/>
+          <h2>Bienvenidos al curso</h2>
+        </div>
+        <h3>Aprendiendo React</h3>
+        <p>Hola mundo</p>
+        <strong>Esto es un strong</strong>
+      </div>
+    );
+  }
 }
 
 export default App;
