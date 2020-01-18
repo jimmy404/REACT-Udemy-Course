d13e3e41f746276cc2a0c2106f1894261f2acdb2
Jimmy <bruno_pier@hotmail.com>
2020-01-17 23:28:18 -0300

    Deleting unnecessary code - App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import React, {Component } from 'react';
 import ConditionalSection from './sections/conditional';
 
-class Contador extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { contador: this.props.contadorInicial }
-    setInterval(() => {
-      this.setState({contador: this.state.contador + 1})
-    }, 1000);
-  }
-
-
-
-
-  render() {
-    return <ContadorNumero numero={this.state.contador} />
-  }
-}
-
-Contador.defaultProps = {
-  contadorInicial: 0
-}
-
-  class ContadorNumero extends Component {
-    render() {
-      console.log('ContadorNumero render()');
-      return <span>{this.props.numero}</span>
-    }
-  }
 
 class App extends Component {
   render(){
     return(
       <div>
-        <p>Primer Componente con state</p>
-        <Contador contadorInicial={100}/>
+        <ConditionalSection />
       </div>
-    )
+    );
   }
 }
 
