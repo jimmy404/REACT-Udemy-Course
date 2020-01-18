667801ea72dcd6bb3ff23103777cfc8fe97566da
Jimmy <bruno_pier@hotmail.com>
2020-01-18 00:25:29 -0300

    Adding forms folder&file - events.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React, { Component } from 'react'
+import Events from './sections/events'
 
-export default class Events extends Component {
-  constructor () {
-    super()
-    this.state = { mouseX: 0, mouseY: 0 }
-    // this.handleMouseMove = this.handleMouseMove.bind(this)
-  }
-
-  handleMouseMove = (e) => {
-    const { clientX, clientY } = e
-    this.setState({ mouseX: clientX, mouseY: clientY })
-  }
-
-  handleClick (e) {
-    console.log(e)
-    console.log(e.nativeEvent)
-    alert('Hi here!')
-  }
-
-  render () {
-    return (
-      <div>
-        <h4>Eventos</h4>
-        <button onClick={this.handleClick}>Hi there!</button>
-        <div
-          onMouseMove={this.handleMouseMove}
-          style={{ border: '1px solid #000', marginTop: 10, padding: 10 }}>
-          <p>{this.state.mouseX}, {this.state.mouseY}</p>
-        </div>
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <Events />
       </div>
     )
   }
 }
+
+export default App;
