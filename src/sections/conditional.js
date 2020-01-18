6255f67360a06dcdb2a7a994da95c36ab25300fd
Jimmy <bruno_pier@hotmail.com>
2020-01-17 23:25:46 -0300

    Adding new components - conditional.js

--- src/sections/conditional.js
+++ src/sections/conditional.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
 
+
+class ComponenteA extends Component {
+    render(){
+        return <p>Componente A</p>
+    }
+}
+class ComponenteB extends Component {
+    render(){
+        return <p>Componente B</p>
+    }
+}
 export default class ConditionalSection extends Component {
     render() {
         return (
